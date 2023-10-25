c198ff5891678e40fa2146d8d8178a8f3938f51b
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2023-10-25 09:27:36 +0300

    technews
    changes

--- script.js
+++ script.js
@@ -22,65 +22,10 @@ menuBtn.addEventListener("click",()=>{
 })
 
 
-let slideImages =  document.querySelectorAll('img');//access the images
-let next = document.querySelector('.next');
-let prev = document.querySelector('.prev');
 
-//access the indicators
-let dots =  document.querySelectorAll('.dot')
+ 
 
-var counter = 0;
-
-//code for next button
-next.addEventListener('click', slideNext);
-function slideNext(){
- slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
- if(counter >= slideImages.length-1){
-    counter = 0;
- }
- else{
-    counter++;
- }
- slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-}
-//code for prev button 
-prev.addEventListener('click', slidePrev);
-function slidePrev(){
- slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
- if(counter == 0){
-    counter = slideImages.length-1;
- }
- else{
-    counter--;
- }
- slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
-}
- //Auto sliding
- function autoSliding(){
-    deletInterval = setInterval(timer, 2500);
-    function timer(){
-        slideNext();
-    }
-}
-autoSliding();
-
-//stop auto sliding when mouse is over
-const container = document.querySelector('.slide-container');
-container.addEventListener('mouseover', function(){
-    clearInterval(deletInterval);
-});
-
-//Resume sliding when mouse is out
-container.addEventListener('mouseout', autoSliding);
-
-
-
-
-
-
-
-
-fetchData(`all`).then(data =>  console.log(data))
+//fetchData(`all`).then(data =>  console.log(data))
 
  const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
