639f157f6a6bb3a3f9e2e5907187802fe9e08d15
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2024-01-26 19:39:56 +0300

    update

---
+++ QUIZ-GAME/js/questions.js
@@ -0,0 +1,62 @@
+//creating an array and passing the number,questions,options and answers
+let questions = [
+    {
+        numb: 1,
+
+        question:"Which is the largest continet on planet Earth?",
+        answer:"Asia",
+        options:[
+            "Europe",
+             "Asia",
+             "Africa",
+             "America"
+
+        ]
+    },
+    {
+        numb: 2,
+        question:"Who was the founder of microsoft company ?",
+        answer:"Bill Gates",
+        options:[
+            "Elon Musk",
+            " Steve Jobs",
+            "Mark Zukerburg",
+            "Bill Gates"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Who was the African leader who was involved in aperthaid",
+        answer:"Nelson Mandela",
+        options:[
+            "Nelson Mandela",
+            "Muamar Gaddafi",
+            "Jomo Kenyatta",
+            "Olesegun Obasanjo"
+
+        ]
+    },
+    {
+        numb: 4,
+        question:"Amazon forest is found in which country?",
+        answer:"Russia",
+        options:[
+            "Australia",
+            "Botswana",
+            "China",
+            "Russia"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Which type of food is both fruit and Vegetable?",
+        answer:"Banana",
+        options:[
+            "Arrow root",
+            "Banana",
+            "Tomato",
+            "Spinach"
+        ]
+    },
+
+];
