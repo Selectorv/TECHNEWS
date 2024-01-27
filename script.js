08f558f6102221c6588ff964bdc95414b9daa774
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2024-01-27 21:26:41 +0300

    updates

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//const API_KEY = "3e032a9e100f432f9b207bc608d2b071"
-//const url =  "https://newsapi.org/v2/everything?q="
-
-
-
-//async function fetchData (query){
-    //const res = await fetch(`${url}${query}&apikey=${API_KEY} `)
-   // const data = await res.json()
-    //return data
-    
-//}
-
-
-////fetchData("ipi2023")
 
 let mobileMenu = document.querySelector(".mobile")
 let menuBtn = document.querySelector(".menuBtn")
