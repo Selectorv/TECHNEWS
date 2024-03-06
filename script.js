10889c195cab62684c2b65f3189151023d6270e8
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2024-03-06 18:55:20 +0300

    updates

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-//let mobileMenu = document.querySelector(".mobile")
-//let menuBtn = document.querySelector(".menuBtn")
-//let menuBtnDisplay = true;
-
-//menuBtn.addEventListener("click",()=>{
-    
-   // mobileMenu.classList.toggle("hidden")
-
-//})
-
-
-
- 
-
-//fetchData(`all`).then(data =>  console.log(data))
 
   const toggleBtn = document.querySelector('.toggle_btn')
   const toggleBtnIcon = document.querySelector('.toggle_btn i')
